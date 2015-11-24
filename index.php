e64fb6f3137c13774d14a37b8a0c7f1b36e01fc6
briannakarle <brianna.risard@gmail.com>
2015-11-24 00:13:28 -0800

    index
    test

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php include ("includes/top.php");?>
+   <!-- <div class="flexslider">
+      <ul class="slides">
+        <li>
+          <img src="images/badjimmys-700.jpg" alt="Bad Jimmy's"/>
+        <li>
+            <img src="images/mellyb-700.jpg" alt="MellyB Wellness"/>
+        </li>
+      </ul>
+    </div>-->
+    <ul class="pgwSlideshow">
+    <li><img src="images/badjimmys-700.jpg" alt="Bad Jimmy's Brewing Co" data-description="HTML5 | CSS3 | PHP | Information Architecture | Wireframes | Visual Designs | Card Sort | UX"></li>
+    <li><img src="images/mellyb-700.jpg" alt="MellyB Wellness" data-description="HTML5 | CSS3 | PHP | Information Architecture | Wireframes | Visual Designs | jQuery"></li>
+
+    <!--<li>
+        <a href="http://en.wikipedia.org/wiki/Monaco" target="_blank">
+            <img src="monaco.jpg" alt="Monaco">
+        </a>
+    </li>-->
+</ul>
+    <div class="infoWrapper">
+        <section class="info">
+            <div class="infoText">
+                <p class="redText">As the world gets more complex, we need to find a way to  make things simple.</p>
+                <p>In 2015, billions of people across the globe use the internet.  Many of those people use it every day, and they come from all walks of life.  Not everyone is going to be internet or technology savvy.  Organizations need to empower the population by having efficient, attractive websites with the every day user in mind. My goal is to create a stress-free web experience, for both user and client. </p>
+            </div>
+        </section>
+    </div>
+    <div class="clearFix"></div>
+    <section class="recentWorkWrapper">
+        <h3>Recent Work</h3>
+        <p>&nbsp;</p>
+        <div class="recentWork">
+            <div class="bjMainPage mainImage">
+                <a href="portfolio.php">
+                    <img class="badjimmys300 img300" src="images/badjimmys300.jpg" alt="Bad Jimmy's Brewing Co">
+                    <div class="bjMainPageInfo">
+                        <p class="projectTitle bjProjectTitle">Bad Jimmy's Brewing Co</p>
+                        <p class="projectInfo bjProjectInfo">Website Redesign &bull; Branding &bull; Responsive Design</p>
+                    </div>
+                </a>
+            </div>
+            <div class="kcMainPage mainImage">
+                <a href="portfolio.php">
+                    <img class="kisscafe300 img300" src="images/kisscafe300.png" alt="Kiss Cafe">
+                    <div class="kcMainPageInfo">
+                        <p class="projectTitle">Kiss Cafe</p>
+                        <p class="projectInfo">Website Redesign &bull; JavaScript/JQuery &bull; Responsive Design</p>
+                    </div>
+                </a>
+            </div>
+            <div class="mbwMainPage mainImage">
+                <a href="portfolio.php">
+                    <img class="mellyb300 img300" src="images/mellyb300.png" alt="MellyB Wellness">
+                    <!--<div class="coverPiece cpmel img300"></div>-->
+                    <div class="mbwMainPageInfo">
+                        <p class="projectTitle">MellyB Wellness</p>
+                        <p class="projectInfo">Website Redesign &bull; Logo Design &bull; JavaScript/JQuery &bull; Responsive Design</p>
+                    </div>
+                </a>
+            </div>
+            <div class="clearFix"></div>
+        </div>
+    </section>
+    <?php include ("includes/bottom.php");?>
